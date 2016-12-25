17fd7c68865df52dbb1788b8ea6feacb6ef5bef7
Thomas Buck <xythobuz@xythobuz.de>
2016-12-25 02:29:50 +0100

    Use CDNs for 3rd party js libs

---
+++ config.h
@@ -0,0 +1,41 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define WEB_PORT 80
+#define BROADCAST_PORT 2390
+#define WEBSOCKET_PORT 2391
+#define NTP_PORT_FROM 2392
+#define NTP_PORT_TO 123
+
+// WiFi Credentials for the captive config portal AP
+#define DEFAULT_SSID "ESP-Weather"
+#define DEFAULT_PASS "testtest"
+
+#define MAX_BROADCAST_WAIT_TIME 550
+#define NTP_RETRY_TIMEOUT 2500
+#define NTP_SERVER_NAME "time.nist.gov"
+#define NTP_PACKET_SIZE 48
+#define UDP_PACKET_BUFFER_SIZE 25
+
+// Size of EEPROM for history storage.
+// 4096 should be ok, but too large values cause system crashes!
+#define EEPROM_SIZE 512
+
+// Broadcast Identifier. Change this when the protocol changes!
+#define UDP_PING_CONTENTS "pingESP8266v0.1"
+#define UDP_ECHO_CONTENTS "echoESP8266v0.1"
+
+// Using the RawGit.com service to serve the scripts directly from GitHub.
+// Consider using cdn.rawgit.com to reduce their server load.
+#define HTML_BEGIN "<html><head><title>ESP-Weather</title>\
+<script src=\"https://code.jquery.com/jquery-3.1.1.min.js\" integrity=\"sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=\" crossorigin=\"anonymous\"></script>\
+<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\
+<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js\" integrity=\"sha256-RASNMNlmRtIreeznffYMDUxBXcMRjijEaaGF/gxT6vw=\" crossorigin=\"anonymous\"></script>\
+<script src=\"https://rawgit.com/xythobuz/ESP-Weather/master/client-script.js\"></script>\
+<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\
+</head><body><script type=\"text/javascript\">"
+
+#define HTML_END "</script></body></html>"
+
+#endif // __CONFIG_H__
+
